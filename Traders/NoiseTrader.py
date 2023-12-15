619ac5c818871fb537aeb6c408f232a3b842f6c5
rishabh.mandayam@gmail.com <141846398+rishabh-mandayam@users.noreply.github.com>
2023-12-14 19:30:44 -0500

    Built Trades abstract class
    Could be modified in the future

---
+++ Traders/NoiseTrader.py
@@ -0,0 +1,11 @@
+import Trader
+import Random
+
+class NoiseTrader(Trader):
+    """
+    NoiseTrader Class: Nickname SillySally
+    Inherets functions from Trader
+    """
+
+    def __init__(self, starting_cash, num_assets):
+
---
+++ Traders/Trader.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class Trader(ABC):
+    """
+    Trader Abstract Class
+
+    All Traders Have the same base functions:
+
+    recieve_spot(): get the current spot prices for each asset
+
+    recieve_time_series(): get time series for each asset
+
+    pricing_algorithm() : compute EV for each asset
+
+    place_order(): Depending on EV calc, place order
+    """
+
+    @abstractmethod
+    def mark_to_market():
+        pass
+
+    @abstractmethod
+    def recieve_spot_prices():
+        pass
+
+    @abstractmethod
+    def recieve_time_series():
+        pass
+
+    @abstractmethod
+    def calculate_expected_value():
+        pass
+
+    @abstractmethod
+    def place_order():
+        pass
